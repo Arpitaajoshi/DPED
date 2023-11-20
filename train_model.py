13c5cb93475af5c72fd49e8223fe6c7a372f3b66
Arpitaajoshi <87361239+Arpitaajoshi@users.noreply.github.com>
2023-11-20 11:55:31 +0530

    train_model.py

--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import imageio
 import numpy as np
 import sys
+from PIL import Image
 
 tf.compat.v1.disable_v2_behavior()
 
@@ -217,14 +218,18 @@ with tf.Graph().as_default(), tf.compat.v1.Session() as sess:
             logs.close()
 
             # save visual results for several test image crops
-
             enhanced_crops = sess.run(enhanced, feed_dict={phone_: test_crops, dslr_: dslr_images, adv_: all_zeros})
-
+ 
             idx = 0
             for crop in enhanced_crops:
                 before_after = np.hstack((np.reshape(test_crops[idx], [PATCH_HEIGHT, PATCH_WIDTH, 3]), crop))
-                imageio.imwrite('results/' + str(phone)+ "_" + str(idx) + '_iteration_' + str(i) + '.jpg', before_after)
-                idx += 1
+ 
+            # Convert the numpy array to a PIL image
+            img = Image.fromarray(before_after.astype('uint8'), 'RGB')
+ 
+            # Save the image
+            img.save('results/' + str(phone) + "_" + str(idx) + '_iteration_' + str(i) + '.jpg')
+            idx += 1
 
             train_loss_gen = 0.0
             train_acc_discrim = 0.0
